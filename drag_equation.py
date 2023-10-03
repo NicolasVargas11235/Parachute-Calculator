79362f007bd553278e17aa47238e70ef98e71ec9
jdtrach <146894556+jdtrach@users.noreply.github.com>
2023-10-03 19:21:43 -0400

    Add files via upload

---
+++ drag_equation.py
@@ -0,0 +1,7 @@
+AIR_DENSITY_KPA = 101.325
+
+def drag(drag_coefficient: float, velocity: float, area: float) -> float:
+    '''Evaluate drag from the drag eqn.
+    '''
+    drag = drag_coefficient * AIR_DENSITY_KPA * velocity ** 2 * area / 2
+    return drag
