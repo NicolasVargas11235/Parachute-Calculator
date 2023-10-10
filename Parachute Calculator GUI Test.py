c4b7c7b4aeccfeb77fb379ac3aace7dbfddf4783
jdtrach <146894556+jdtrach@users.noreply.github.com>
2023-10-10 15:14:33 -0400

    Update Parachute Calculator GUI Test.py
    Changed .pack() placement to .grid() for all elements. Added header bar.

--- Parachute Calculator GUI Test.py
+++ Parachute Calculator GUI Test.py
@@ -28,34 +28,38 @@ def calculate_parachute():
     result_label.config(text=f"Required Parachute Size: {parachute_size:.2f} square meters")
 
 
-"""Main Script, Written by: Nicolas Vargas, Last Modified: 2023-10-07"""
+"""Main Script, Written by: Nicolas Vargas, Last Modified: 2023-10-10 by Jordan Trach"""
 # Create the main window
 root = tk.Tk()
 root.title("Parachute Calculator")
 
+#Create and place title header
+label_header = tk.Label(root, text = "Parachute Calculator", bg = "white")
+label_header.grid(column = 0, row = 0, columnspan = 10, sticky = (tk.N, tk.S, tk.E, tk.W))
+
 # Create and place labels and respective entry widgets
 label_mass = tk.Label(root, text="Enter Load Mass (kg):")
-label_mass.pack()
+label_mass.grid(column = 1, row = 1, ipadx = 5)
 entry_mass = tk.Entry(root)
-entry_mass.pack()
+entry_mass.grid(column = 2, row = 1, ipadx = 5)
 
 label_velocity = tk.Label(root, text="Enter Descent Velocity (m/s):")
-label_velocity.pack()
+label_velocity.grid(column = 1, row = 2, ipadx = 5)
 entry_velocity = tk.Entry(root)
-entry_velocity.pack()
+entry_velocity.grid(column = 2, row = 2, ipadx = 5)
 
 label_drag_coefficient = tk.Label(root, text="Enter Drag Coefficient:")
-label_drag_coefficient.pack()
+label_drag_coefficient.grid(column = 1, row = 3, ipadx = 5)
 entry_drag_coefficient = tk.Entry(root)
-entry_drag_coefficient.pack()
+entry_drag_coefficient.grid(column = 2, row = 3, ipadx = 5)
 
 # Create and place a button to calculate
 calculate_button = tk.Button(root, text="Calculate Parachute Size", command=calculate_parachute)
-calculate_button.pack()
+calculate_button.grid(column = 1, row = 4, ipadx = 5)
 
 # Create and place a label to display the result
 result_label = tk.Label(root, text="")
-result_label.pack()
+result_label.grid(column = 3, row = 1, rowspan = 3, ipadx = 5)
 
 # Start the GUI event loop
 root.mainloop()
