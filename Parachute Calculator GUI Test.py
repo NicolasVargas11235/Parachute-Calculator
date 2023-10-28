b25cb7a2dbee80fc4506eaa7c84adefebef181b1
jdtrach <146894556+jdtrach@users.noreply.github.com>
2023-10-28 11:13:09 -0400

    Update Parachute Calculator GUI Test.py
    Open for review - porting in the basic modular gui code while I continue to work on logistics for a larger number of equations.

--- Parachute Calculator GUI Test.py
+++ Parachute Calculator GUI Test.py
@@ -28,38 +28,56 @@ def calculate_parachute():
     result_label.config(text=f"Required Parachute Size: {parachute_size:.2f} square meters")
 
 
-"""Main Script, Written by: Nicolas Vargas, Last Modified: 2023-10-10 by Jordan Trach"""
+"""Main Script, Written by: Nicolas Vargas, Last Modified: 2023-10-28 by Jordan Trach"""
 # Create the main window
 root = tk.Tk()
 root.title("Parachute Calculator")
 
-#Create and place title header
-label_header = tk.Label(root, text = "Parachute Calculator", bg = "white")
-label_header.grid(column = 0, row = 0, columnspan = 10, sticky = (tk.N, tk.S, tk.E, tk.W))
+# Create and place title header
+header_frame = ttk.Frame(root)
+header_frame['borderwidth'] = 5
+header_frame['relief'] = 'raised'
+header_frame['height'] = 20
+header_frame.grid(column = 0, row = 0, columnspan = 15, sticky = (tk.E, tk.W))
+label_header = tk.Label(header_frame, text = "Parachute Calculator", )
+label_header.grid(column = 1, row = 0)
 
-# Create and place labels and respective entry widgets
-label_mass = tk.Label(root, text="Enter Load Mass (kg):")
-label_mass.grid(column = 1, row = 1, ipadx = 5)
+# Create and place sidebar for sought value
+seeking_header = tk.Label(root, text = "Which value do you need?")
+seeking_header.grid(column = 0, row = 1, columnspan = 2, padx = 5, pady = 5)
+
+#Create and place value options (placeholders)
+parachute_size_button = tk.Button(root, text = "Parachute Size", command=calculate_parachute)
+parachute_size_button.grid(column = 0, row = 2, sticky = (tk.N, tk.S, tk.E, tk.W))
+
+# Create and place main entry window for given values
+given_frame = ttk.Frame(root)
+given_frame['height'] = 20
+given_frame.grid(column = 2, row = 1, columnspan = 3, sticky = (tk.E, tk.W))
+given_header = tk.Label(root, text = "Enter known values:")
+given_header.grid(column = 2, row = 1, columnspan = 5)
+
+# Create and place given values table
 entry_mass = tk.Entry(root)
-entry_mass.grid(column = 2, row = 1, ipadx = 5)
+entry_mass.grid(column = 2, row = 2, ipadx = 5, padx = 5)
+label_mass = tk.Label(root, text="Load Mass (kg)")
+label_mass.grid(column = 2, row = 3, ipadx = 5)
 
-label_velocity = tk.Label(root, text="Enter Descent Velocity (m/s):")
-label_velocity.grid(column = 1, row = 2, ipadx = 5)
 entry_velocity = tk.Entry(root)
-entry_velocity.grid(column = 2, row = 2, ipadx = 5)
+entry_velocity.grid(column = 3, row = 2, ipadx = 5, padx = 5)
+label_velocity = tk.Label(root, text="Descent Velocity (m/s)")
+label_velocity.grid(column = 3, row = 3, ipadx = 5)
 
-label_drag_coefficient = tk.Label(root, text="Enter Drag Coefficient:")
-label_drag_coefficient.grid(column = 1, row = 3, ipadx = 5)
 entry_drag_coefficient = tk.Entry(root)
-entry_drag_coefficient.grid(column = 2, row = 3, ipadx = 5)
+entry_drag_coefficient.grid(column = 4, row = 2, ipadx = 5, padx = 5)
+label_drag_coefficient = tk.Label(root, text="Drag Coefficient")
+label_drag_coefficient.grid(column = 4, row = 3, ipadx = 5)
 
-# Create and place a button to calculate
-calculate_button = tk.Button(root, text="Calculate Parachute Size", command=calculate_parachute)
-calculate_button.grid(column = 1, row = 4, ipadx = 5)
-
-# Create and place a label to display the result
-result_label = tk.Label(root, text="")
-result_label.grid(column = 3, row = 1, rowspan = 3, ipadx = 5)
+# Create and place sidebar for results
+result_header = tk.Label(root, text = "Result")
+result_header.grid(column = 5, row = 1, columnspan = 3, sticky = (tk.N, tk.S, tk.E, tk.W))
+result_label = tk.Label(root)
+result_label.grid(column = 5, row = 2)
 
 # Start the GUI event loop
 root.mainloop()
