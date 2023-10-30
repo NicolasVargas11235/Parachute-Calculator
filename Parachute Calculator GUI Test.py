94a194447e62c1ef046e84589203a5f87eeac67f
frankngo05 <146895148+frankngo05@users.noreply.github.com>
2023-10-30 19:07:48 -0400

    Update Parachute Calculator GUI Test.py

--- Parachute Calculator GUI Test.py
+++ Parachute Calculator GUI Test.py
@@ -27,6 +27,45 @@ def calculate_parachute():
     # Update the result label
     result_label.config(text=f"Required Parachute Size: {parachute_size:.2f} square meters")
 
+"""Equations for Stencil Calculation"""
+def calculate_radius(area):
+    radius = (area/pi)**0.5
+    return radius
+
+
+def calculate_stencil_base_size(area):
+    """Calculates the base of stencil assuming that it 
+    equals one eighth of circumference of the parachute skirt.
+    
+    Written by: Frank Ngo
+    Last Modified: 2023-10-30
+    """    
+    
+    
+    radius = calculate_radius (area)
+    base_size = float
+    base_size = pi*(radius)/(4)
+    return base_size
+
+def calculate_stencil_height_size(area):
+    """Calculates the height of stencil assuming that it is 2.25*(base_size). 
+    Multiplier of 2.25 comes from ratios of the base size with respect to 
+    radius"
+    
+    Written by: Frank Ngo
+    Last Modified: 2023-10-30
+    """   
+   
+   
+    radius = calculate_radius (area)
+    base_height = float
+    base_height = 9*pi*(radius)/(16)
+    return base_height
+
+"""Arbitary area"""
+area = 5 
+print ("The stensil base size should be: " + str(calculate_stencil_base_size(area)))
+print ("The stencil height size should be: " + str(calculate_stencil_height_size(area)))
 
 # Create the main window
 root = tk.Tk()
